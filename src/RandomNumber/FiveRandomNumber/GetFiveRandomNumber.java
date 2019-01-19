73090b3384d4a2f0bfc3d23df5e52845b4a9809b
liupeng <LIUPENG.0@outlook.com>
2019-01-19 22:29:04 +0800

    Java 四位-五位-六位:随机数 V 1.0.0

---
+++ src/RandomNumber/FiveRandomNumber/GetFiveRandomNumber.java
@@ -0,0 +1,46 @@
+package RandomNumber.FiveRandomNumber;
+
+import RandomNumber.NumberTable;
+
+public class GetFiveRandomNumber {
+
+    private String randomNumbers = null;
+
+    private NumberTable numberTable = null;
+
+    public String getRandomNumbers() {
+        return randomNumbers;
+    }
+
+    public void setRandomNumbers(String randomNumbers) {
+        this.randomNumbers = randomNumbers;
+    }
+
+    public NumberTable getNumberTable() {
+        return numberTable;
+    }
+
+    public void setNumberTable(NumberTable numberTable) {
+        this.numberTable = numberTable;
+    }
+
+    public void createNumberTable() {
+        this.numberTable = new NumberTable(5);
+    }
+
+    public void init() {
+        this.randomNumbers = new String();
+        this.createNumberTable();
+    }
+
+    public GetFiveRandomNumber() {
+        this.init();
+        this.setRandomNumbers(this.getNumberTable().getRandomNumber());
+    }
+
+    public static void main(String[] args) {
+        GetFiveRandomNumber getFiveRandomNumber = new GetFiveRandomNumber();
+        String randomNumbers = getFiveRandomNumber.getRandomNumbers();
+        System.out.print(randomNumbers);
+    }
+}
