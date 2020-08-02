37cabd681fbf4fd7df1cb31c1d711f985cbc74a0
liupeng <liupeng.0@outlook.com>
2020-08-02 21:33:33 +0800

    加密小工具

---
+++ src/encrypt/index.java
@@ -0,0 +1,21 @@
+package encrypt;
+
+/**
+ * @文件名 encrypt
+ * @描述
+ * @创建人 liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间 2020/8/2 - 9:29 下午
+ * @修改人和其它信息
+ */
+public class index {
+
+    public static void main(String[] args) {
+        String name = "liupeng";
+        String password = "123456";
+        String userID = "liupeng.0@outlook.com";
+        String IDPassword = "123456";
+
+
+    }
+}
