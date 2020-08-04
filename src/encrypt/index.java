b6625896b994af303e9a0fc39e9d745457c89b87
liupeng <liupeng.0@outlook.com>
2020-08-04 23:57:14 +0800

    加密小工具

--- src/encrypt/index.java
+++ src/encrypt/index.java
@@ -1,5 +1,12 @@
 package encrypt;
 
+import encrypt.fileIO.FileRead;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @文件名 encrypt
  * @描述
@@ -10,12 +17,27 @@ package encrypt;
  */
 public class index {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String name = "liupeng";
         String password = "123456";
         String userID = "liupeng.0@outlook.com";
         String IDPassword = "123456";
+        FileRead fileRead = new FileRead();
+        String s = "000";
+        s = readFile("/Users/liupeng/IdeaProjects/JavaTools/src/encrypt/userName.txt");
+        System.out.println(s);
 
 
+    }
+
+    public static String readFile(String pathname) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(pathname));
+        String line;
+        //网友推荐更加简洁的写法
+        while ((line = br.readLine()) != null) {
+            // 一次读入一行数据
+            System.out.println(line);
+        }
+        return line;
     }
 }
