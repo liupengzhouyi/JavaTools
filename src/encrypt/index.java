910b79134ae8cf4abac6cf9d5661427559074043
liupeng <liupeng.0@outlook.com>
2020-08-11 22:46:32 +0800

    加密小工具

--- src/encrypt/index.java
+++ src/encrypt/index.java
@@ -22,39 +22,11 @@ public class index {
         String IDPassword = "123456";
         FileRead fileRead = new FileRead();
         String s = "000";
-        s = fileRead.readFile("src/encrypt/userName.txt");
+        s = fileRead.readFile("src/encrypt/userInformation/userName.txt");
         System.out.println(s);
-        writeFile();
 
         FileInput fileInput = new FileInput();
         fileInput.Input("src/encrypt/userInformation/passwordValue.txt", password);
 
-    }
-
-    public static String readFile(String pathname) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(pathname));
-        String line;
-        //网友推荐更加简洁的写法
-        while ((line = br.readLine()) != null) {
-            // 一次读入一行数据
-            System.out.println(line);
-        }
-        return line;
-    }
-
-    public static void writeFile() {
-        try {
-            File writeName = new File("src/encrypt/userName.txt"); // 相对路径，如果没有则要建立一个新的output.txt文件
-            writeName.createNewFile(); // 创建新文件,有同名的文件的话直接覆盖
-            try (FileWriter writer = new FileWriter(writeName);
-                 BufferedWriter out = new BufferedWriter(writer)
-            ) {
-                out.write("我会写入文件啦1\r\n"); // \r\n即为换行
-                out.write("我会写入文件啦2\r\n"); // \r\n即为换行
-                out.flush(); // 把缓存区内容压入文件
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
