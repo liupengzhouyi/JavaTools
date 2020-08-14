5f4881bcd99b0ddfe24aedcedf92e8cdc6a88b2b
liupeng <liupeng.0@outlook.com>
2020-08-14 23:55:23 +0800

    加密小工具

---
+++ src/encrypt/savePassword/Password/Password.java
@@ -0,0 +1,9 @@
+package encrypt.savePassword.Password;/**
+ * @文件名 encrypt.savePassword.Password
+ * @描述 
+ * @创建人  liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间  2020/8/14 - 11:53 下午
+ * @修改人和其它信息
+ */public class Password {
+}
---
+++ src/encrypt/savePassword/PasswordInformation/PasswordInformation.java
@@ -0,0 +1,9 @@
+package encrypt.savePassword.PasswordInformation;/**
+ * @文件名 encrypt.savePassword.PasswordInformation
+ * @描述 
+ * @创建人  liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间  2020/8/14 - 11:52 下午
+ * @修改人和其它信息
+ */public class PasswordInformation {
+}
