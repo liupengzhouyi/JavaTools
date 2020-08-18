437c51714f1ee75c71064e016b01754fd2f571b7
liupeng <liupeng.0@outlook.com>
2020-08-18 22:33:58 +0800

    Socket

---
+++ src/Socket/Client.java
@@ -0,0 +1,45 @@
+package Socket;
+
+/**
+ * @文件名 Socket
+ * @描述
+ * @创建人 liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间 2020/8/18 - 10:31 下午
+ * @修改人和其它信息
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("127.0.0.1",8888);
+
+            //构建IO
+            InputStream is = s.getInputStream();
+            OutputStream os = s.getOutputStream();
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+            //向服务器端发送一条消息
+            bw.write("测试客户端和服务器通信，服务器接收到消息返回到客户端\n");
+            bw.flush();
+
+            //读取服务器返回的消息
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String mess = br.readLine();
+            System.out.println("服务器："+mess);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
